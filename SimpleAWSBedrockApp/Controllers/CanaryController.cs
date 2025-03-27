0911939e5d5e9fa9abe0621f92dce55dcd11ed5e
billysoomro <billy.soomro02@gmail.com>
2025-03-27 21:30:26 +0000

    Adding canary controller and adding Amazon.Lambda.AspNetCoreServer.Hosting package

---
+++ SimpleAWSBedrockApp/Controllers/CanaryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAWSBedrockApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CanaryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("The SimpleAWSBedrockApp is running.");
+        }
+    }
+}
--- SimpleAWSBedrockApp/Startup.cs
+++ SimpleAWSBedrockApp/Startup.cs
@@ -27,6 +27,8 @@ namespace SimpleAWSBedrockApp
             });
 
             services.AddSingleton<AwsBedrockService>();
+
+            services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
